2968d8024426566e59c8dc7a533d7ba309b7cdb5
sevaslavik <seva5000Pro@gmail.com>
2019-09-02 17:12:11 +0300

    width opacity

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
 	var accordeon = $('.js-acordeon');
 
     accordeon.on('click', function() {
+    	$(this).find('img').toggleClass('opacity');
         $(this).find('.content__description').toggleClass('is-active');
         $(this).find('.learn-more').toggleClass('link-active');
     });
