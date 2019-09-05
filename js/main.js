13fd05512653768db84119ee23589424c79cd7d8
sevaslavik <seva5000Pro@gmail.com>
2019-09-05 13:33:00 +0300

    second work prototype

--- js/main.js
+++ js/main.js
@@ -4,9 +4,17 @@
 	var accordeon = $('.js-acordeon');
 
     accordeon.on('click', function() {
-    	$(this).find('img').toggleClass('opacity');
+    	//$(this).find('img').toggleClass('opacity');
         $(this).find('.content__description').toggleClass('is-active');
         $(this).find('.learn-more').toggleClass('link-active');
+    });
+
+    accordeon.on('mouseover', function() {
+		$(this).find('img').addClass('opacity');
+    });
+
+    accordeon.on('mouseout', function() {
+	$(this).find('img').removeClass('opacity');
     });
 
 }(jQuery));
